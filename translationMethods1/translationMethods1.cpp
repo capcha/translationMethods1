b612218f10b68fb503391960100c46762ac55aa8
capcha <arjen.robben2012@yandex.ru>
2021-02-23 18:28:31 +0700

    Создали структуры таблиц

--- translationMethods1/translationMethods1.cpp
+++ translationMethods1/translationMethods1.cpp
@@ -1,20 +1,199 @@
-﻿// translationMethods1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <vector>
+#include <string>
 
-#include <iostream>
+using namespace std;
+
+struct Operator {
+   char name;
+
+   bool operator < (const Operator& b) const {
+      return name < b.name;
+   }
+
+   bool operator > (const Operator& b) const {
+      return name > b.name;
+   }
+
+   bool operator == (Operator b) {
+      return name == b.name;
+   }
+};
+
+struct Separator {
+   char name;
+
+   bool operator < (const Operator& b) const {
+      return name < b.name;
+   }
+
+   bool operator > (const Operator& b) const {
+      return name > b.name;
+   }
+
+   bool operator == (Operator b) {
+      return name == b.name;
+   }
+};
+
+struct ReservedWord {
+   string name;
+   bool operator < (const ReservedWord& b) const {
+      return name < b.name;
+   }
+
+   bool operator > (const ReservedWord& b) const {
+      return name > b.name;
+   }
+
+   bool operator == (ReservedWord b) {
+      return name == b.name;
+   }
+};
+
+template<typename T> struct ImmutableTable {
+   vector<T> data;
+
+   ImmutableTable(int dataSize) {
+      data.resize(dataSize);
+   }
+
+   bool getElementById(int id, string &result) {
+      
+      if (data.size() < id) {
+         result = data[id];
+         return true;
+      }
+
+      return false;
+   }
+
+   bool isExist(string dataString) {
+
+      if (find(data.begin(), data.end(), dataString) != data.end()) {
+         return true;
+      }
+
+      return false;
+   }
+
+   bool getIdByElement(string elementName, int &result) {
+
+      if (isExist(dataString)) {
+         for (int i = 0; i < data.size(); i++) {
+            if (data[i] == elementName) {
+               result = i;
+               break;
+            }
+         }
+         return true;
+      }
+
+      return false;
+   }
+
+};
+
+struct Constant {
+   string name; 
+   int id; 
+   string value; 
+
+   bool operator < (const Constant& b) const {
+      return name < b.name;
+   }
+
+   bool operator > (const Constant& b) const {
+      return name > b.name;
+   }
+
+   bool operator == (Constant b) {
+      return name == b.name;
+   }
+};
+
+struct Int {
+   string name;
+   int id;
+   string value;
+   bool isInit;
+
+   bool operator < (const Int& b) const {
+      return name < b.name;
+   }
+
+   bool operator > (const Int& b) const {
+      return name > b.name;
+   }
+
+   bool operator == (Int b) {
+      return name == b.name;
+   }
+};
+
+template<typename T> struct MutableTable {
+   vector<T> data;
+
+   bool addElement(string element, bool value) {
+      try
+      {
+         data.push_back(element);
+      }
+      catch (const exception&)
+      {
+         cerr << "Ошибка добавления";
+         return false; 
+      }
+
+      return true;
+   }
+
+   bool getElementById(int id, string &result) {
+      if (data.size() < id) {
+         result = data[id];
+         return true;
+      }
+
+      return false;
+   }
+
+   bool isExist(string dataString) {
+
+      if (find(data.begin(), data.end(), dataString) != data.end()) {
+         return true;
+      }
+
+      return false;
+   }
+
+   bool getIdByElement(string elementName, int& result) {
+
+      if (isExist(dataString)) {
+         for (int i = 0; i < data.size(); i++) {
+            if (data[i] == elementName) {
+               result = i;
+               break;
+            }
+         }
+         return true;
+      }
+
+      return false;
+   }
+
+
+   bool getIsInitById(int id, string& result) {
+      if (data.size() < id) {
+         result = isInitWithValue[id];
+         return true;
+      }
+
+      return false;
+   }
+     
+};
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
