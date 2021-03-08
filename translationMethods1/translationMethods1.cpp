0f24fca28a7fec3e0c0aa044110cb2b6736e9b44
capcha <arjen.robben2012@yandex.ru>
2021-03-08 14:01:02 +0700

    Изменил структуры на классы

--- translationMethods1/translationMethods1.cpp
+++ translationMethods1/translationMethods1.cpp
@@ -7,307 +7,327 @@ using namespace std;
 
 class Operator {
    public:
-      char getName();
+      char getName() {
+         return name;
+      }
+      
+      Operator() = default;
+
+      Operator(char symbol) : name(symbol) {} 
+      Operator(string symbol) : name(symbol[0]) {}
+
+      bool operator < (Operator & b) const {
+         return name < b.getName();
+      }
+
+      bool operator > (Operator & b) const {
+         return name > b.getName();
+      }
+
+      bool operator == (Operator b) {
+         return name == b.getName();
+      }
 
    private: 
       char name;
-
-   Operator() = default;
-
-   Operator(char symbol) : name(symbol){}
-   Operator(string symbol) : name(symbol[0]){}
-
-   bool operator < (Operator& b) const {
-      return name < b.getName();
-   }
-
-   bool operator > (Operator& b) const {
-      return name > b.getName();
-   }
-
-   bool operator == (Operator b) {
-      return name == b.getName();
-   }
 };
-
-char Operator::getName() {
-   return name;
-}
 
 class AlphabetUnit {
    public:
-      char getName();
+      char getName() {
+         return name;
+      }
+      
+      AlphabetUnit() = default;
+
+      AlphabetUnit(char symbol) : name(symbol) {}
+      AlphabetUnit(string symbol) : name(symbol[0]) {}
+
+
+      bool operator < (AlphabetUnit & b) const {
+         return name < b.getName();
+      }
+
+      bool operator > (AlphabetUnit & b) const {
+         return name > b.getName();
+      }
+
+      bool operator == (AlphabetUnit b) {
+         return name == b.getName();
+      }
 
    private: 
       char name;
-
-   AlphabetUnit() = default;
-
-   AlphabetUnit(char symbol) : name(symbol) {}
-   AlphabetUnit(string symbol) : name(symbol[0]) {}
-
-
-   bool operator < (AlphabetUnit& b) const {
-      return name < b.getName();
-   }
-
-   bool operator > (AlphabetUnit& b) const {
-      return name > b.getName();
-   }
-
-   bool operator == (AlphabetUnit b) {
-      return name == b.getName();
-   }
 };
-
-char AlphabetUnit::getName() {
-   return name;
-}
 
 class Separator {
    public:
-      char getName();
+      char getName() {
+         return name;
+      }
+
+      Separator() = default;
+
+      Separator(char symbol) : name(symbol) {}
+      Separator(string symbol) : name(symbol[0]) {}
+
+      bool operator < (Separator & b) const {
+         return name < b.getName();
+      }
+
+      bool operator > (Separator & b) const {
+         return name > b.getName();
+      }
+
+      bool operator == (Separator b) {
+         return name == b.getName();
+      }
 
    private:
       char name;
-
-   Separator() = default;
-
-   Separator(char symbol) : name(symbol) {}
-   Separator(string symbol) : name(symbol[0]) {}
-
-   bool operator < (Separator& b) const {
-      return name < b.getName();
-   }
-
-   bool operator > (Separator& b) const {
-      return name > b.getName();
-   }
-
-   bool operator == (Separator b) {
-      return name == b.getName();
-   }
 };
 
-char Separator::getName() {
-   return name;
-}
-
-struct ReservedWord {
+class ReservedWord {
    public:
-      string getName();
+      string getName() {
+         return name;
+      }
+
+      ReservedWord() = default;
+
+      ReservedWord(string _name) {
+         name = _name;
+      }
+
+      bool operator < (ReservedWord & b) const {
+         return name < b.getName();
+      }
+
+      bool operator > (ReservedWord & b) const {
+         return name > b.getName();
+      }
+
+      bool operator == (ReservedWord b) {
+         return name == b.getName();
+      }
 
    private:
       string name;
-
-   ReservedWord() = default;
-
-   ReservedWord(string _name) {
-      name = _name;
-   }
-
-   bool operator < (ReservedWord& b) const {
-      return name < b.getName();
-   }
-
-   bool operator > (ReservedWord& b) const {
-      return name > b.getName();
-   }
-
-   bool operator == (ReservedWord b) {
-      return name == b.getName();
-   }
 };
 
-string ReservedWord::getName() {
-   return name;
-}
+class Constant {
+   public:
+      string getName() {
+         return name;
+      }
 
-struct Constant {
-   string name;
-   int value;
+      int getValue() {
+         return value;
+      }
 
-   Constant() = default;
+      Constant() = default;
 
-   Constant(string _name, string _value) {
-      name = _name;
-      value = atoi(_value.c_str());
-   }
-      
-   Constant(string _name, int _value) {
-      name = _name;
-      value = _value;
-   }
+      Constant(string _name, string _value) {
+         name = _name;
+         value = atoi(_value.c_str());
+      }
 
-   bool operator < (const Constant& b) const {
-      return name < b.name;
-   }
+      Constant(string _name, int _value) {
+         name = _name;
+         value = _value;
+      }
 
-   bool operator > (const Constant& b) const {
-      return name > b.name;
-   }
+      bool operator < (Constant& b) const {
+         return name < b.getName();
+      }
 
-   bool operator == (Constant b) {
-      return name == b.name && value == b.value;
-   }
+      bool operator > (Constant& b) const {
+         return name > b.getName();
+      }
+
+      bool operator == (Constant b) {
+         return name == b.getName() && value == b.getValue();
+      }
+
+   private:
+      string name;
+      int value;
 };
 
-struct Int {
-   string name;
-   int value;
-   bool isInit;
+class Int {
+   public:
+      string getName() {
+         return name;
+      }
 
-   Int() = default;
+      int getValue() {
+         return value;
+      }
 
-   Int(string _name, string _value) {
-      name = _name;
-      value = atoi(_value.c_str());
-      isInit = (value != -858993460) ? true : false;
-   }
+      bool getIsInit() {
+         return isInit;
+      }
 
-   Int(string _name, int _value) {
-      name = _name;
-      value = _value;
-      isInit = (value != -858993460) ? true : false;
-   }
+      Int() = default;
 
-   bool operator < (const Int& b) const {
-      return name < b.name;
-   }
+      Int(string _name, string _value) {
+         name = _name;
+         value = atoi(_value.c_str());
+         isInit = (value != -858993460) ? true : false;
+      }
 
-   bool operator > (const Int& b) const {
-      return name > b.name;
-   }
+      Int(string _name, int _value) {
+         name = _name;
+         value = _value;
+         isInit = (value != -858993460) ? true : false;
+      }
 
-   bool operator == (Int b) {
-      return name == b.name && value == b.value;
-   }
+      bool operator < (Int& b) const {
+         return name < b.getName();
+      }
+
+      bool operator > (Int& b) const {
+         return name > b.getName();
+      }
+
+      bool operator == (Int b) {
+         return name == b.getName() && value == b.getValue();
+      }
+
+   private:
+      string name;
+      int value;
+      bool isInit;
 };
 
-template<typename T> struct ImmutableTable {
-   vector<T> data;
+template<typename T> class ImmutableTable {
 
-   ImmutableTable(string filename) {
-      int size;
-      T elem;
-      string buf;
-      ifstream fIn(filename);
+   public:
+      ImmutableTable(string filename) {
+         int size;
+         T elem;
+         string buf;
+         ifstream fIn(filename);
 
-      fIn >> size;
-      data.resize(size);
+         fIn >> size;
+         data.resize(size);
 
-      for (int i = 0; i < size; i++) {
-         fIn >> buf;
-         elem = T(buf);
-         data[i] = elem;
-      }
-      
-      fIn.close();
-   }
-
-   bool getElementById(int id, T &result) {
-      
-      if (data.size() > id) {
-         result = data[id];
-         return true;
-      }
-
-      return false;
-   }
-
-   bool isExist(T element) {
-
-      if (find(data.begin(), data.end(), element) != data.end()) {
-         return true;
-      }
-
-      return false;
-   }
-
-   bool getIdByElement(T element, int &result) {
-
-      if (isExist(element)) {
-         for (int i = 0; i < data.size(); i++) {
-            if (data[i] == element) {
-               result = i;
-               break;
-            }
+         for (int i = 0; i < size; i++) {
+            fIn >> buf;
+            elem = T(buf);
+            data[i] = elem;
          }
-         return true;
+
+         fIn.close();
       }
 
-      return false;
-   }
+      bool getElementById(int id, T& result) {
 
-};
-\ 
-template<typename T> struct MutableTable {
-   vector<T> data;
-
-   MutableTable(string filename) {
-      int size;
-      string name, value;
-      T elem;
-      ifstream fIn(filename);
-
-      fIn >> size;
-
-      for (int i = 0; i < size; i++) {
-         fIn >> name >> value;
-         elem = T(name, value);
-         data.push_back(elem);
-      }
-
-      fIn.close();
-   }
-
-   bool addElement(T element) {
-      try
-      {
-         data.push_back(element);
-      }
-      catch (const exception&)
-      {
-         cerr << "Ошибка добавления";
-         return false; 
-      }
-
-      return true;
-   }
-
-   bool getElementById(int id, T &result) {
-      if (data.size() > id) {
-         result = data[id];
-         return true;
-      }
-
-      return false;
-   }
-
-   bool isExist(T element) {
-
-      if (find(data.begin(), data.end(), element) != data.end()) {
-         return true;
-      }
-
-      return false;
-   }
-
-   bool getIdByElement(T element, int& result) {
-
-      if (isExist(element)) {
-         for (int i = 0; i < data.size(); i++) {
-            if (data[i] == element) {
-               result = i;
-               break;
-            }
+         if (data.size() > id) {
+            result = data[id];
+            return true;
          }
+
+         return false;
+      }
+
+      bool isExist(T& element) {
+
+         if (find(data.begin(), data.end(), element) != data.end()) {
+            return true;
+         }
+
+         return false;
+      }
+
+      bool getIdByElement(T element, int& result) {
+
+         if (isExist(element)) {
+            for (int i = 0; i < data.size(); i++) {
+               if (data[i] == element) {
+                  result = i;
+                  break;
+               }
+            }
+            return true;
+         }
+
+         return false;
+      }
+
+   private:
+      vector<T> data;
+};
+
+template<typename T> class MutableTable {
+
+   public:
+      MutableTable(string filename) {
+         int size;
+         string name, value;
+         T elem;
+         ifstream fIn(filename);
+
+         fIn >> size;
+
+         for (int i = 0; i < size; i++) {
+            fIn >> name >> value;
+            elem = T(name, value);
+            data.push_back(elem);
+         }
+
+         fIn.close();
+      }
+
+      bool addElement(T element) {
+         try
+         {
+            data.push_back(element);
+         }
+         catch (const exception&)
+         {
+            cerr << "Ошибка добавления";
+            return false;
+         }
+
          return true;
       }
 
-      return false;
-   }
-     
+      bool getElementById(int id, T& result) {
+         if (data.size() > id) {
+            result = data[id];
+            return true;
+         }
+
+         return false;
+      }
+
+      bool isExist(T element) {
+
+         if (find(data.begin(), data.end(), element) != data.end()) {
+            return true;
+         }
+
+         return false;
+      }
+
+      bool getIdByElement(T element, int& result) {
+
+         if (isExist(element)) {
+            for (int i = 0; i < data.size(); i++) {
+               if (data[i] == element) {
+                  result = i;
+                  break;
+               }
+            }
+            return true;
+         }
+
+         return false;
+      }
+
+   private:
+      vector<T> data;
 };
 
 int main()
